9ec61155fffba99303b639bf541f29039cce959a
Patrisha2209 <75588741+Patrisha2209@users.noreply.github.com>
2020-12-07 00:11:41 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,5 @@
+ 
+print("ievadiet gatavošanas komponentu")
+komponent=input()
+print("iekšā:")
+print("ielieciet tīru un mazgātu "+komponent+ " pannāsu "+  "pievieno cukuru pēc svara, vāriet divas stundas līdz mīkstumam")
